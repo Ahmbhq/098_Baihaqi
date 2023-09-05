237d51aa21cbc34a713d3a6ec60b5bb1e03dcdca
Ahmbhq <143860460+Ahmbhq@users.noreply.github.com>
2023-09-05 22:28:04 +0700

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+#menghitung balok
+print ("menghitung balok") 
+p = int(input("panjang :"))
+l =  int (input("lebar :"))
+t = int(input("tinggi :"))
+volume = p*l*t
+print("volumme balok : ",volume)
---
+++ penugasan volume tabung.py
@@ -0,0 +1,7 @@
+#menghitung volume tabung
+print("menghitung volume tabung")
+pi = 3.14
+r = int(input("jari-jari tabung :"))
+t = int(input("tingi tabung : "))
+v = pi*r**2*t
+print("volume tabung",v)
